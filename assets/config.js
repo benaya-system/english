e06cb6e19f651f577c926aaaf3669eb02ffc655c
benaya-system <bruno@advir.org>
2026-03-26 20:50:39 -0300

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyFHeLOTh3UzSaDdM1QeKJDgdVNVJrYg9eBGRDh-jsVEDV_9vXXigCaBZTknMiBC3luDw/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxJWCtkUOewSnj_cgw7Y4KC421RmXCusr9mVvMe-LrzGci0XHmApC0g55nvLNI1kkAXCA/exec',
   DOWNLOAD_BASE_PATH: 'download/',
   CACHE_TTL_MS: 180000,
   REFRESH_INTERVAL_MS: 90000
